e627c2ef94b1d6d0c83f74298eec69127919f0de
Volodymyr Zhukov <volodymyr.zhukov@elixirator.com>
2025-02-27 17:03:59 +0200

    add local storage for user preferences

--- frontend/src/contexts/UserPreferencesContext.jsx
+++ frontend/src/contexts/UserPreferencesContext.jsx
@@ -5,16 +5,23 @@ export const DISPLAY_STYLES = {
   grid: "grid",
 };
 
+const LOCAL_STORAGE_KEY = "userPreferences";
+
 const UserPreferencesContext = createContext();
 
 export const UserPreferencesProvider = ({ children }) => {
-  const [displayStyle, setDisplayStyle] = useState(DISPLAY_STYLES.list);
+  const [displayStyle, setDisplayStyle] = useState(() => {
+    const savedPreferences = localStorage.getItem(LOCAL_STORAGE_KEY);
+    return savedPreferences || DISPLAY_STYLES.list;
+  });
 
   function setList() {
+    localStorage.setItem(LOCAL_STORAGE_KEY, DISPLAY_STYLES.list);
     setDisplayStyle(DISPLAY_STYLES.list);
   }
 
   function setGrid() {
+    localStorage.setItem(LOCAL_STORAGE_KEY, DISPLAY_STYLES.grid);
     setDisplayStyle(DISPLAY_STYLES.grid);
   }
 
