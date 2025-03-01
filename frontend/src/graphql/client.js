18dc9e2cfd6ca3f864b03266bf9654b7d792b1be
Volodymyr Zhukov <volodymyr.zhukov@elixirator.com>
2025-03-01 19:58:03 +0200

    remove commented apollo config, unused hook

--- frontend/src/graphql/client.js
+++ frontend/src/graphql/client.js
@@ -33,12 +33,3 @@ const client = new ApolloClient({
 
 export default client;
 
-// OLD Setup before subscription
-// import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
-
-// const client = new ApolloClient({
-//   link: new HttpLink({ uri: "http://localhost:3000/graphql" }),
-//   cache: new InMemoryCache(),
-// });
-
-// export default client;
--- frontend/src/hooks/useGetProjects.js
+++
@@ -1,8 +0,0 @@
-import { useQuery } from "@apollo/client";
-import { GET_PROJECTS } from "../queries.js";
-
-export const useGetProject = () => {
-  const { data, loading, error } = useQuery(GET_PROJECTS);
-
-  return { users: data?.users, loading, error };
-};
